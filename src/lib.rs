be27de5f83505eeb52ab4dac0e5e7d85c5a6b602
h4llow3En <mail@felixdoering.com>
2017-04-10 17:25:49 +0200

    add markdown export

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ extern crate libc;
 
 use std::{slice, io, ptr};
 
+
 /// Generates a well formated String of all undone Todos
 pub fn gen_tasks_mail(tdo: &tdo_core::tdo::Tdo) -> Option<String> {
     let mut listed = String::new();
@@ -32,9 +33,44 @@ pub fn gen_tasks_mail(tdo: &tdo_core::tdo::Tdo) -> Option<String> {
     }
 }
 
+/// Generates a markdown String to export the lists.
+pub fn gen_tasks_md(tdo: &tdo_core::tdo::Tdo, list_done: bool) -> Option<String> {
+    let mut markdown = String::from("# Your tasks\n\n");
+    let name = get_full_name();
+    if name.is_ok() {
+        markdown.push_str(&format!("Here are the tasks for {}\n\n", &name.unwrap()));
+    }
+    let mut intern = String::new();
+    for list in tdo.to_owned().lists.iter() {
+        let tasks: Vec<tdo_core::todo::Todo>;
+        if list_done {
+            tasks = list.list.to_owned();
+        } else {
+            tasks = list.list_undone();
+        }
+        if tasks.len() > 0 {
+            intern.push_str(&format!("\n# {}\n", &list.name));
+            for entry in tasks {
+                if entry.done {
+                    intern.push_str(&format!("- [x] {}\n", &entry.name));
+                } else {
+                    intern.push_str(&format!("- [ ] {}\n", &entry.name));
+                }
+            }
+        }
+    }
+    match intern.len() {
+        0 => None,
+        _ => {
+            markdown.push_str(&intern);
+            Some(markdown)
+        },
+    }
+}
+
 /// Returns the full Name of the current user if present.
 #[allow(unsafe_code)]
-pub fn get_full_name() -> Result<String, io::Error>{
+pub fn get_full_name() -> Result<String, io::Error> {
     unsafe {
         let uid = libc::geteuid();
         let user = ptr::read(libc::getpwuid(uid));
